b48a24199e78d99ebeb2a0348ccf5c208cc755ce
Basdiani <95381619+Basdiani@users.noreply.github.com>
2023-09-06 00:30:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,6 @@ function preflight_checks {
         exit -1
     fi
 
-    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F 'klipper.service')" ]; then
-        printf "[PRE-CHECK] Klipper service found! Continuing...\n\n"
-   
     if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F 'klipper-1.service')" ]; then
         printf "[PRE-CHECK] Klipper service found! Continuing...\n\n"
    
